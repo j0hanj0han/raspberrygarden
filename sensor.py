235e0e50432cedb0c42e364abe5fda9c2dec4b5d
j0hanj0han <johan.chapelain@gmail.com>
2025-03-22 08:28:59 +0100

    [works] conf ok

--- sensor.py
+++ sensor.py
@@ -1,11 +1,18 @@
+#!/usr/bin/env python3
+
 import bme680
 import time
 import json
+import sys
 
 try:
     sensor = bme680.BME680(bme680.I2C_ADDR_PRIMARY)
 except (RuntimeError, IOError):
-    sensor = bme680.BME680(bme680.I2C_ADDR_SECONDARY)
+    try:
+        sensor = bme680.BME680(bme680.I2C_ADDR_SECONDARY)
+    except (RuntimeError, IOError) as e:
+        print(json.dumps({"error": "Impossible de se connecter au capteur BME680: " + str(e)}))
+        sys.exit(1)
 
 # Configuration du capteur
 sensor.set_humidity_oversample(bme680.OS_2X)
@@ -13,18 +20,17 @@ sensor.set_pressure_oversample(bme680.OS_4X)
 sensor.set_temperature_oversample(bme680.OS_8X)
 sensor.set_filter(bme680.FILTER_SIZE_3)
 
-while True:
-    try:
-        if sensor.get_sensor_data():
-            data = {
-                "temperature": round(sensor.data.temperature, 2),
-                "humidity": round(sensor.data.humidity, 2),
-                "pressure": round(sensor.data.pressure, 2)
-            }
-            print(json.dumps(data))
-        
-        time.sleep(5)
-        
-    except Exception as e:
-        print(json.dumps({"error": str(e)}))
-        time.sleep(5)
+try:
+    if sensor.get_sensor_data():
+        data = {
+            "temperature": round(sensor.data.temperature, 2),
+            "humidity": round(sensor.data.humidity, 2),
+            "pressure": round(sensor.data.pressure, 2)
+        }
+        print(json.dumps(data))
+    else:
+        print(json.dumps({"error": "Impossible de lire les données du capteur"}))
+        sys.exit(1)
+except Exception as e:
+    print(json.dumps({"error": str(e)}))
+    sys.exit(1)
